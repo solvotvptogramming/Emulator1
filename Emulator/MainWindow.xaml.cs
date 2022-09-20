608f2e8f6ff8f0cceeff5010402e8bb461381601
solvotvptogramming <uvv0106@gmail.com>
2022-09-20 19:57:20 +0800

    init

---
+++ Emulator/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Emulator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Gimnazia1Entities db;
+        public MainWindow()
+        {
+            InitializeComponent();
+             
+            db = new Gimnazia1Entities();
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.Cursor = Cursors.Wait;
+            string Event = "32";
+            // p.logdata
+            Events codeStruct = new Events();
+            {
+               Events codeStruct2 = new Events();
+                codeStruct2 = new Events();
+               
+
+
+            }
+            
+        }   
+    }
+}
